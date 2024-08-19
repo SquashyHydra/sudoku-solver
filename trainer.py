e0c716acdbed54eb1adbdbb243bf3fd0d364232c
SquashyHydra <squashyhydra@gmail.com>
2024-08-19 11:14:17 +0000

    Aug 19, 2024, 1:14 PM

--- trainer.py
+++ trainer.py
@@ -1,8 +1,9 @@
 import tensorflow as tf
 from tensorflow.keras.datasets import mnist
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Flatten
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Dense, Flatten, Dropout
 from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
 # Load MNIST data
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
@@ -13,10 +14,19 @@ x_test = x_test.astype('float32') / 255.0
 y_train = to_categorical(y_train, 10)
 y_test = to_categorical(y_test, 10)
 
-# Build model
+# Build CNN model
 model = Sequential([
-    Flatten(input_shape=(28, 28)),
+    Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)),
+    MaxPooling2D((2, 2)),
+    Dropout(0.25),
+
+    Conv2D(64, (3, 3), activation='relu'),
+    MaxPooling2D((2, 2)),
+    Dropout(0.25),
+
+    Flatten(),
     Dense(128, activation='relu'),
+    Dropout(0.5),
     Dense(10, activation='softmax')
 ])
 
