a6dd5a176f7068f0eede6a3dc66fcaff6607cb87
SquashyHydra <squashyhydra@gmail.com>
2024-08-19 13:45:44 +0000

    Aug 19, 2024, 3:45 PM

--- sudoku_DataGenerationv1.py
+++ sudoku_DataGenerationv1.py
@@ -1,10 +1,7 @@
 import numpy as np
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
 
 class Sudoku_DataGeneration:
     def __init__(self):
-        self.model = RandomForestClassifier()
         self.data = []
         self.labels = []
 
@@ -47,22 +44,6 @@ class Sudoku_DataGeneration:
                 grid[row][col] = 0
 
         return False
-
-    def find_empty_cell(self, grid):
-        for i in range(9):
-            for j in range(9):
-                if grid[i][j] == 0:
-                    return (i, j)
-        return None
-
-    def train_model(self):
-        X_train, X_test, y_train, y_test = train_test_split(
-            np.array(self.data), np.array(self.labels), test_size=0.2, random_state=42)
-        self.model.fit(X_train, y_train)
-        print(f"Model accuracy: {self.model.score(X_test, y_test)}")
-
-    def predict_next_move(self, grid):
-        return self.model.predict(grid.flatten().reshape(1, -1))
 
 # Example Usage
 Sudoku_DataGeneration.generate_sudoku_data(n_samples=10000)  # Generate synthetic data
