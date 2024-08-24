61476a61cacebf0bac346a2f42ecab45894c8453
SquashyHydra <squashyhydra@gmail.com>
2024-08-24 05:27:11 +0000

    Aug 24, 2024, 7:27 AM

--- datajoink.py
+++ datajoink.py
@@ -1,7 +1,10 @@
+import json
 from datasets import load_dataset
 
 ds = load_dataset("Ritvik19/Sudoku-Dataset", split='train')
 
+def format_sudoku(sudoku_string):
+    return [[int(sudoku_string[i * 9 + j]) for j in range(9)] for i in range(9)]
 
 for puzzle in ds['puzzle']:
-    print(puzzle)
+    print(format_sudoku(puzzle))
